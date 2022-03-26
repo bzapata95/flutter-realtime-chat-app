9a208d9cf801bc23d0208de63efe8db9cdbdbbbb
Bryan Zapata <bmzc95@gmail.com>
2022-03-26 13:17:08 -0500

    validate token

--- lib/screens/users_screen.dart
+++ lib/screens/users_screen.dart
@@ -1,5 +1,7 @@
 import 'package:chat_app/models/user.dart';
+import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class UsersScreen extends StatefulWidget {
@@ -21,15 +23,20 @@ class _UsersScreenState extends State<UsersScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
     return Scaffold(
       appBar: AppBar(
-          title:
-              const Text('Mi nombre', style: TextStyle(color: Colors.black87)),
+          title: Text(authService.user.name,
+              style: const TextStyle(color: Colors.black87)),
           elevation: 1,
           backgroundColor: Colors.white,
           leading: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.exit_to_app, color: Colors.black87)),
+            icon: const Icon(Icons.exit_to_app, color: Colors.black87),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, 'login');
+              AuthService.deleteToken();
+            },
+          ),
           actions: [
             Container(
               margin: const EdgeInsets.only(right: 10),
